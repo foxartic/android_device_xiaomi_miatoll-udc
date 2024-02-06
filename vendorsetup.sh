af339418aa916e35b3261d3f95be98739206ef66
athizz2005 <atharva15.bgmi@gmail.com>
2024-02-06 10:18:51 +0000

    miatoll : update vendorsetup for dolby

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/c0smic-lab/vendor_xiaomi_miatoll -b 14 vendor/xiaomi/miatoll  --depth=1
+git clone https://github.com/project-dynamic/android_vendor_xiaomi_miatoll-udc -b 14 vendor/xiaomi/miatoll  --depth=1
 git clone https://github.com/c0smic-Lab/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250  --depth=1
 git clone https://gitlab.com/crdroidandroid/prebuilts_clang_host_linux-x86_clang-r510928 --depth=1 prebuilts/clang/host/linux-x86/clang-r510928
 git clone https://gitlab.com/athizz2005/android_vendor_MiuiCamera -b leica-5.0 vendor/xiaomi/miuicamera --depth=1
